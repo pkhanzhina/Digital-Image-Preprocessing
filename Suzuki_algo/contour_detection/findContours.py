96f69f488e1e030e78afde0a9a6cbc8c293c6ebb
pkhanzhina <khanzhina.polina@icloud.com>
2021-10-01 01:11:17 +0300

    fix FindContours._border_detection()

--- Suzuki_algo/contour_detection/findContours.py
+++ Suzuki_algo/contour_detection/findContours.py
@@ -35,18 +35,17 @@ class FindContours:
         p2, p3 = p1, p
         while True:
             p4 = self._get_nonzero_pixel(p3, p2, f, clockwise=False)
-            if p4 is None:
-                break
             if p3[1] < w - 1:
                 if f[p3[0], p3[1] + 1] == 0:
                     f[p3[0], p3[1]] = -NBD
                 elif f[p3[0], p3[1]] == 1:
                     f[p3[0], p3[1]] = NBD
                 cnt.append(p3)
+            if p4 is None:
+                break
             if p4[0] == p[0] and p4[1] == p[1] and p3[0] == p1[0] and p3[1] == p1[1]:
                 break
             p2, p3 = p3, p4
-            # cnt.append(p2)
         return np.array(cnt)
 
     def _get_nonzero_pixel(self, start_point, work_point, f, clockwise: bool):
