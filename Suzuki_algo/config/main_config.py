10bd494a9d4d3088ea7c807531b11abf91714437
pkhanzhina <khanzhina.polina@icloud.com>
2021-10-07 21:44:30 +0300

    update HSV interval

--- Suzuki_algo/config/main_config.py
+++ Suzuki_algo/config/main_config.py
@@ -4,7 +4,7 @@ cfg = EasyDict()
 
 cfg.hue_interval = (40, 88)
 cfg.saturation_interval = (88, 163)
-cfg.value_interval = (12, 140)
+cfg.value_interval = (0, 140)
 
 cfg.min_area = 10000
 cfg.max_area = None
